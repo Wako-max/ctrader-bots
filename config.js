65881e77e3a8accd6d938b9f8919dc7e210ace8b
Wako-max <sanderduinkerken93@gmail.com>
2026-08-09 04:10:20 +0200

    Connect Daginfo TV to Google Sheet

---
+++ config.js
@@ -0,0 +1,7 @@
+window.DAGINFO_CONFIG = {
+  title: 'Daginfo',
+  subtitle: 'Algemene ruimte',
+  sheetCsvUrl: 'https://docs.google.com/spreadsheets/d/1vlYbGZkMIqbI_Hg_e_qxnY2N1HmtuT2Rc3dgV7fTdyQ/gviz/tq?tqx=out:csv&sheet=Daginfo',
+  refreshSeconds: 60,
+  includeAlwaysRows: true
+};
