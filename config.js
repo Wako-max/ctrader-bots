8055511a055f1e968b58718c571593e2cb67377a
Wako-max <sanderduinkerken93@gmail.com>
2026-08-09 06:22:13 +0200

    Use private-sheet sync data source

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 window.DAGINFO_CONFIG = {
-  title: 'Daginfo',
-  subtitle: 'Algemene ruimte',
-  sheetCsvUrl: 'https://docs.google.com/spreadsheets/d/1vlYbGZkMIqbI_Hg_e_qxnY2N1HmtuT2Rc3dgV7fTdyQ/gviz/tq?tqx=out:csv&sheet=Daginfo',
+  dataUrl: 'data.json',
   refreshSeconds: 60,
   includeAlwaysRows: true
 };
